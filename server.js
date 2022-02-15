6a2752359ab57bfcfacd656382ae65179a8babbd
Daniel Ayesoro <dayesoro@gmail.com>
2022-02-15 20:11:37 +0000

    updated static string

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(function (req, res, next) {
 //static middleware that returns lesson image
 app.use(function (req, res, next) {
     // Uses path.join to find the path where the file should be
-    var filePath = path.join(__dirname, 'stati', req.url);
+    var filePath = path.join(__dirname, 'static', req.url);
     // Built-in fs.stat gets info about a file
     fs.stat(filePath, function (err, fileInfo) {
         if (err) {
